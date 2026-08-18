0626c13772c8366af1699776be95e5660e5a1a65
IamAshishMani <ashishmani238100@gmail.com>
2026-08-18 10:21:10 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+import http from 'http';
+
+const server = http.createServer();
+server.on('request', (req, res) => {
+  res.write("Welcome to Amazon Shopping 🛍️\n");
+  res.write("Nodemon is running and watching for changes...\n");
+  res.end();
+});
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
