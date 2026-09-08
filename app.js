ac0032128dc87a99a2b3e172ea29f139ad0c6be6
IamAshishMani <ashishmani238100@gmail.com>
2026-09-08 10:06:14 +0530

    Helper Method

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 import http from 'http';
-import * as teams from './Teams/teams.js';
+import * as teams from './teams.js';
+
+const sendJSON = (res, statusCode, data) => {
+  res.writeHead(statusCode, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify(data));
+}
+
+const parseJsonBody = (req) => {
+  return new Promise((resolve, reject) => {
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk.toString();
+    });
+    req.on('end', () => {
+      try {
+        resolve(body ? JSON.parse(body) : {});
+      } catch (error) {
+        reject(error);
+      }
+    });
+  });
+};
+
 const server = http.createServer((req, res) => {
   if (req.url === '/' && req.method === 'GET') {
-    const teams = teams.getAllTeams();
-    res.write(JSON.stringify(teams));
+    const teamsList = teams.getAllTeams();
+    sendJSON(res, 200, teamsList);
   }
   else {
-    res.statusCode = 404;
-    res.write('Not Found');
-    res.end();
+    sendJSON(res, 404, { error: 'Not Found' });
   }
 });
 server.listen(5000, () => {
