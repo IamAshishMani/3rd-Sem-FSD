a637cc8d940d376e75dfccbe2122a3feebda14d1
IamAshishMani <ashishmani238100@gmail.com>
2026-09-08 09:52:28 +0530

    app.js

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 import http from 'http';
 import * as teams from './Teams/teams.js';
-const server = http.createServer();
-server.on('request', (req, res) => {
-  res.write("Welcome to Amazon Shopping 🛍️\n");
-  res.write("Nodemon is running and watching for changes...\n");
-  res.end();
+const server = http.createServer((req, res) => {
+  if (req.url === '/' && req.method === 'GET') {
+    const teams = teams.getAllTeams();
+    res.write(JSON.stringify(teams));
+  }
+  else {
+    res.statusCode = 404;
+    res.write('Not Found');
+    res.end();
+  }
 });
 server.listen(5000, () => {
   console.log(`Server is running on http://localhost:5000`);
